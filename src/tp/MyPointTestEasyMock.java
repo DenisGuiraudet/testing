81a299c59a0ef828d6a499d27a7ab3a10c48378c
Denis <guiraudet@live.fr>
2018-10-03 09:52:23 +0200

    Cleaning easymock import

--- src/tp/MyPointTestEasyMock.java
+++ src/tp/MyPointTestEasyMock.java
@@ -1,9 +1,12 @@
 package tp;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class MyPointTest {
+class MyPointTestEasyMock {
 
     private MyPoint myPointEmpty, myPointFilled;
 
