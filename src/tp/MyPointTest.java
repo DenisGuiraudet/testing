4d520511a2524bce91df89d01a914f19c86e9727
Denis <guiraudet@live.fr>
2018-10-03 09:48:58 +0200

    Import of easymock lib

--- src/tp/MyPointTest.java
+++ src/tp/MyPointTest.java
@@ -3,6 +3,9 @@ package tp;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.easymock.EasyMock.*;
+import org.easymock.*;
+
 class MyPointTest {
 
     private MyPoint myPointEmpty, myPointFilled;
