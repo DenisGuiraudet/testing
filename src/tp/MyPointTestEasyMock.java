f2da6c17bc7c59b8b149b17b601b431298191529
Denis <guiraudet@live.fr>
2018-10-03 10:29:47 +0200

    import of JUnit 4

--- src/tp/MyPointTestEasyMock.java
+++ src/tp/MyPointTestEasyMock.java
@@ -1,79 +1,38 @@
 package tp;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.*;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.easymock.*;
+import static org.easymock.EasyMock.*;
 
-class MyPointTestEasyMock {
+import java.util.Random;
 
+public class MyPointTestEasyMock extends EasyMockSupport {
+
+    @Rule
+    public EasyMockRule rule = new EasyMockRule(this);
+
+    @Mock
     private MyPoint myPointEmpty, myPointFilled;
 
-    @BeforeEach
+    @Before
     void setUp() {
         myPointEmpty = new MyPoint();
         myPointFilled = new MyPoint(11.1, 12.2);
     }
 
-    @AfterEach
+    @After
     void tearDown() {
     }
 
     @Test
-    void testGetEmpty() {
-        assertEquals(0d, myPointEmpty.getX(), 0.0001);
-        assertEquals(0d, myPointEmpty.getY(), 0.0001);
-    }
-
-    @Test
-    void testGetFilled() {
-        assertEquals(11.1, myPointFilled.getX(), 0.0001);
-        assertEquals( 12.2, myPointFilled.getY(), 0.0001);
-    }
-
-    @Test
-    void testSetGetX() {
-        myPointEmpty.setX(13.3);
-        assertEquals(13.3, myPointEmpty.getX(), 0.0001);
-    }
-
-    @Test
-    void testSetGetY() {
-        myPointEmpty.setY(14.4);
-        assertEquals(14.4, myPointEmpty.getY(), 0.0001);
-    }
-
-    @Test
-    void testDoubleNaN() {
-        myPointEmpty.setX(Double.NaN);
-        assertNotEquals(Double.NaN, myPointEmpty.getX());
-    }
-
-    @Test
-    void testScale() {
-        int factor = 7;
-        MyPoint myNewPoint = myPointFilled.scale(factor);
-        assertEquals(myPointFilled.getX() * factor,
-                myNewPoint.getX(), 0.0001);
-        assertEquals(myPointFilled.getY() * factor,
-                myNewPoint.getY(), 0.0001);
-    }
-
-    @Test
-    void testHorizontalSymmetry() {
-        MyPoint myNewPoint = myPointFilled.horizontalSymmetry(myPointEmpty);
-        assertEquals((myPointFilled.getX() + myPointEmpty.getX()) / 2,
-                myNewPoint.getX(), 0.0001);
-    }
-
-    @Test
-    void testCentralSymmetryNULL () {
-        assertThrows(IllegalArgumentException.class,
-                () -> {
-                    MyPoint myNewPoint = new MyPoint(10, 20).centralSymmetry(null);
-                });
-        ;
+    void testSetPoint() {
+        Random randomX = new Random();
+        Random randomY = new Random();
+        myPointEmpty.setPoint(randomX, randomY);
+        assertEquals(randomX.nextDouble(), myPointEmpty.getX(), 0.0001);
+        assertEquals(randomY.nextDouble(), myPointEmpty.getY(), 0.0001);
     }
 
 }
